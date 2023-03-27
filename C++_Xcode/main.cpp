30e70913c477640b29f529ea6845b043fa46abdd
vous00 <100505460+vous00@users.noreply.github.com>
2023-03-27 20:35:55 +0900

    Initial Commit

---
+++ C++_Xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++_Xcode
+//
+//  Created by vous00 on 2023/03/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
